9d9037655ddb98a8c799158d55c11cddb4a024b5
Dawid Paszko <dawid.paszko@gmail.com>
2016-02-06 18:09:43 +0100

    updated vote script

--- vote.php
+++ vote.php
@@ -4,7 +4,5 @@ require('init.php');
 
 //set vote
 if(isset($_POST['video_id']) && isset($_POST['rate'])){
-	$_SESSION[ $_POST['video_id'] ] = $_POST['rate'];
+	updateRating($_POST['videoId'],$_POST['rate']);
 }
-
-var_dump(getNextVideo());
