a3a688b06a645741d14e1a4abc0cf7509691294c
Matheus Boschetti <matheus.boschetti@king.com>
2016-02-07 10:17:37 +0100

    Fixing page title

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@ $fpdo = new FluentPDO($pdo);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Admin</title>
+    <title>Admin Page</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
